750d9479f3afc3c6182bb25b87e5106b81629ec3
Motta <mottabotaqbota@gmail.com>
2024-10-11 12:01:57 -0300

    Commit: Finalização do Header padrão e criação das estilizações básicas. Adicionada a logo Larica do dia

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,13 +1,16 @@
 import "./header.css"
+import Logo from '../../logo_full_horizontal.svg';
+
 
 export default function Header() {
     return (
-        <header>
-            <img src="https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcTwmGmPbJzpDBmP6tkwVqdOTWackE-i8Ytc-MACtUVE5icHW8Xi0uzzUm_GnSTlmu1KlXdYL2ELKWA971PKCg&s=19" alt="" className="logo" />
-            <nav>
-                <a href="#"><i class="fa-solid fa-house"></i>Início</a>
-                <a href="#"><i class="fa-solid fa-book-open"></i>Receitas</a>
-                <a href="#"><i class="fa-solid fa-drumstick-bite"></i>Suas Receitas</a>
+        <header className="header">
+            <img src={Logo} alt="Logo Larica do dia posicionada com o Icone em formato de sorriso com a língua para fora, do lado esquerdo, e na direita do icone está a tipografia Larica do dia - Receitas culinárias." className="logo" />
+            <button>Ver receita completa</button>
+            <nav className="navigationLinks">
+                <a href="#" className="navLink"><i class="fa-solid fa-house"></i>Início</a>
+                <a href="#" className="navLink"><i class="fa-solid fa-book-open"></i>Receitas</a>
+                <a href="#" className="navLink"><i class="fa-solid fa-drumstick-bite"></i>Suas Receitas</a>
             </nav>
         </header>
     )
