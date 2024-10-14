f35a8cfe2b089a99b062025e3a59cd606b3a6a95
Motta <mottabotaqbota@gmail.com>
2024-10-14 01:47:33 -0300

    Completo o Header, criado o carrossel de imagens.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './basicStyles.css';
 import Header from './components/header/header.jsx';
+import Carroussel from './components/carroussel/carroussel.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Carroussel></Carroussel>
     </div>
   );
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,11 @@
 import "./header.css"
-import Logo from '../../logo_full_horizontal.svg';
-
+import Logo from '../../logotypes/logo_full_horizontal.svg';
+ 
 
 export default function Header() {
     return (
         <header className="header">
             <img src={Logo} alt="Logo Larica do dia posicionada com o Icone em formato de sorriso com a língua para fora, do lado esquerdo, e na direita do icone está a tipografia Larica do dia - Receitas culinárias." className="logo" />
-            <button>Ver receita completa</button>
             <nav className="navigationLinks">
                 <a href="#" className="navLink"><i class="fa-solid fa-house"></i>Início</a>
                 <a href="#" className="navLink"><i class="fa-solid fa-book-open"></i>Receitas</a>
